a01abcfd260d1a7f5b765a96a710ede98f247312
svtslv <svtslv7@gmail.com>
2020-03-04 15:43:07 +0200

    0.0.7

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,16 @@ import { argv } from 'optimist';
 import { runServer } from './webdav-cli.server';
 import { getHelp, getVersion } from './webdav-cli.utils';
 
+const selfSignedKey = __dirname + '/../certs/self-signed.key.pem';
+const selfSignedCert = __dirname + '/../certs/self-signed.cert.pem';
 const path = process.env.WEBDAV_CLI_PATH || argv.path || process.cwd();
 const port = process.env.WEBDAV_CLI_PORT || argv.port || 1900;
 const host = process.env.WEBDAV_CLI_HOST || argv.host || '127.0.0.1';
 const username = process.env.WEBDAV_CLI_USERNAME || argv.username || Math.random().toString(36).replace(/[^a-z]+/g, '');
 const password = process.env.WEBDAV_CLI_PASSWORD || argv.password || Math.random().toString(36).replace(/[^a-z]+/g, '');
 const ssl = process.env.WEBDAV_CLI_SSL || argv.ssl || false;
-const sslKey = fs.readFileSync(process.env.WEBDAV_CLI_SSL_KEY || argv.sslKey || __dirname + '/../certs/self-signed.key.pem').toString();
-const sslCert = fs.readFileSync(process.env.WEBDAV_CLI_SSL_CERT || argv.sslCert || __dirname + '/../certs/self-signed.cert.pem').toString();
+const sslKey = fs.readFileSync(process.env.WEBDAV_CLI_SSL_KEY || argv.sslKey || selfSignedKey).toString();
+const sslCert = fs.readFileSync(process.env.WEBDAV_CLI_SSL_CERT || argv.sslCert || selfSignedCert).toString();
 
 console.log(chalk.green(figlet.textSync('webdav-cli', { horizontalLayout: 'full' })));
 
